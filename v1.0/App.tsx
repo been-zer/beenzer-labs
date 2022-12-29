cbbaa50d857d035480f6c256cee098ea8da01848
Vincent Porret <vincent.porret@hotmail.com>
2022-12-29 11:41:20 +0100

    login v0

---
+++ v1.0/App.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './screens/login';
+
+
+const Stack = createNativeStackNavigator();
+
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
